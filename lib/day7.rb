252e9452768f2d1b112a474700c91b1d106bf427
nyahehnagi <bromley.jones@gmail.com>
2021-12-07 11:19:01 +0000

    optimised with a look up for fuel used at any distance

--- lib/day7.rb
+++ lib/day7.rb
@@ -2,10 +2,10 @@ crab_data = File.readlines('data/inputday7.txt').first.chomp.split(",").map!{|e|
 
 number_of_crabs_at_each_point =  crab_data.group_by(&:itself).map { |k,v| [k, v.count] }.to_h
 
+#min and max values for the horizontal positions
 min_x = crab_data.min
 min_y = crab_data.max
 
-p (min_x..min_y)
 
 def fuel_used(distance)
     counter = 0
@@ -13,11 +13,14 @@ def fuel_used(distance)
     return counter
 end
 
-def fuel_to_point(position, number_of_crabs_at_each_point)
-    number_of_crabs_at_each_point.map { |crab_position, multiplier| fuel_used((crab_position - position).abs) *  multiplier }.sum
+# create look up for fuel used for the range of distances available
+fuel_look_up = (min_x..min_y).map{|distance| [distance, fuel_used(distance)]}.to_h
+
+def fuel_to_point(position, number_of_crabs_at_each_point, fuel_look_up)
+    number_of_crabs_at_each_point.map { |crab_position, multiplier| fuel_look_up[(crab_position - position).abs] *  multiplier }.sum
 end
 
-distance_to_points = (min_x..min_y).map {|position| fuel_to_point(position, number_of_crabs_at_each_point)}
+distance_to_points = (min_x..min_y).map {|position| fuel_to_point(position, number_of_crabs_at_each_point, fuel_look_up)}
 
 p distance_to_points.min
 
