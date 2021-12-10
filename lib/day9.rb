a98979f4978d37650e0fc097f2cb210152357de0
nyahehnagi <bromley.jones@gmail.com>
2021-12-10 12:58:46 +0000

    day 9 part 2 - messy recursion

--- lib/day9.rb
+++ lib/day9.rb
@@ -5,13 +5,13 @@ require 'benchmark'
 include FileManagement
 
 #create array of arrays
-height_map =  getData("data/inputday9test.txt").map{|row| row.split("").map(&:to_i)}
+$height_map =  getData("data/inputday9.txt").map{|row| row.split("").map(&:to_i)}
 
 # hash look up for checking data that conforms
 low_points = Hash.new()
 
 #look at each row and see if criteria of adjacents are higher is true
-height_map.each_with_index do |row, row_index|
+$height_map.each_with_index do |row, row_index|
     row.each_with_index do |height, col_index|
         # make sure we are not at the end of the array
         last_index = row.rindex(row.last)
@@ -40,7 +40,7 @@ height_map.each_with_index do |row, row_index|
     end
 end
 
-transposed_heights =  height_map.transpose
+transposed_heights =  $height_map.transpose
 
 #transpose and check adjacency
 transposed_heights.each_with_index do |row, row_index|
@@ -76,16 +76,94 @@ transposed_heights.each_with_index do |row, row_index|
     end
 end
 
+
 p low_points
 p low_points.map{|k,v| v + 1}.sum
 
+#MESSY RECURSION :) Needs some serious tidying up
 # PART 2
+heights_visited = Hash.new()
+$max_y = $height_map.rindex($height_map.last)
+$max_x = $height_map[0].rindex($height_map[0].last)
 
+def basin_count(location, visited)
 
+    y_value = location[0]
+    x_value = location[1]
+    
+    store_location = false
 
+    p "Location is : #{location}"
+    p  "Locations visited so far before check #{visited}"
+    #p x_value
+    #p y_value
+    # have we been here before, if so, exit
+    if visited.key?(location) == true
+        p "We've been here before. Exiting function"
+        return visited
+    end
 
+    visited[[y_value,x_value]] = 1
+    p  "Locations visited so far #{visited}"
 
+    #Look Up
+    if y_value - 1 >= 0
+        next_y = y_value - 1
+        if $height_map[next_y][x_value] < 9  
+            p "Looking Up Check"
+            store_location = true
+            #go take a look at the next location
+            basin_count([next_y,x_value],visited)
+        end
+    end
+    #Look Right
+    if x_value + 1 <= $max_x
+        p "Looking Right Check"
+        next_x = x_value + 1
+        p "Value to the right is #{$height_map[y_value][next_x]}"
+        p "Y : #{y_value} -  X : #{next_x}"
+        if $height_map[y_value][next_x] < 9 && visited
+            store_location = true
+            #go take a look at the next location
+            basin_count([y_value,next_x],visited)
+        else
+            p "Item to Right is 9"
+        end
+    end
+    #Look left
+    if x_value - 1 >= 0
+        p "Looking Left Check"
+        next_x = x_value - 1
+        p "Value to the left is #{$height_map[y_value][next_x]}"
+        if $height_map[y_value][next_x] < 9
+            p "Going Left Y : #{y_value} -  X : #{next_x}"
+            store_location = true
+            #go take a look at the next location
+            basin_count([y_value,next_x],visited)
+        else
+            p "Item to left is less than 0"
+        end
+    end
+    #Look Down
+    if y_value + 1 <= $max_y
+        p "Looking Down Check"
+        next_y = y_value + 1
+        if $height_map[next_y][x_value] < 9
+            #go take a look at the next location
+            basin_count([next_y,x_value],visited)
+        end
+    end
 
+    return visited
+end
+
+#p basin_count([4,6],heights_visited).count
+
+p low_points.map{|point, value| basin_count(point,Hash.new()).count}.sort.last(3)
+
+#.inject(1){|sum, item| sum *= item }
+
+#[99, 111, 115]
 =begin
 2199943210
 3987894921
